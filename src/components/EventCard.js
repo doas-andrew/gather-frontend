383b99b70b7f0765f95d23c55417eb34d9b7822d
Andrew Allen <asallen67@gmail.com>
2019-08-23 19:21:23 -0400

    entire EventCard links to event page

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -9,13 +9,12 @@ import DateDiv from '../components/DateDiv'
 export default function EventCard (props) {
 	let { event } = props
 	return (
-		<div className='EventCard' style={{backgroundImage: `url(${getRandomImage(event.category)})` }}>
-			<DateDiv size='small' date={new Date(event.date)} color='dodgerBlue' />
-			<div className='stats'><span>{event.num_attendees}</span> <FaUser/></div>
-
-			<Link to={'/events/'+event.id}>
+		<Link to={'/events/'+event.id} style={{ color: 'Black' }}>
+			<div className='EventCard' style={{backgroundImage: `url(${getRandomImage(event.category)})` }}>
+				<DateDiv size='small' date={new Date(event.date)} color='dodgerBlue' />
+				<div className='stats'><span>{event.num_attendees}</span> <FaUser/></div>
 				<h1 className='title'>{event.title}</h1>
-			</Link>
-		</div>
+			</div>
+		</Link>
 	)
 }
